c0fdc1876a7b7bbb414cb2e881df27033da2306d
tyeh11 <tyeh75@gmail.com>
2017-05-26 14:20:27 -0700

    Build Host and Leader

--- src/src/Host.java
+++ src/src/Host.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Created by TC_Yeh on 5/26/2017.
  */
 public class Host {
+    private StateManager stateManager;
+    //need to store public keys of other hosts
+    private ServerSocket aServer;
+    private Leader leader;
+    private Follower follower;
+    private Candidate candidate;
+
+    Host() throws IOException {
+        stateManager = new StateManager();
+        aServer = new ServerSocket();
+        leader = new Leader();
+        follower = new Follower();
+        candidate = new Candidate();
+
+    }
+
+
+    static public void main(String args[]){
+
+    }
+
 
 }
