8d9e39c6f35e7ce1ec966bad61190b69ee67425a
tyeh11 <tyeh75@gmail.com>
2017-05-29 09:43:30 -0700

    Add host package

---
+++ src/src/host/State.java
@@ -0,0 +1,67 @@
+package host;
+
+/**
+ * Created by TC_Yeh on 5/26/2017.
+ */
+public class State {
+    private String stateName;
+    private int stateValue;
+
+    /**
+     * This constructor builds instance with input value.
+     * @param stateName stateName
+     * @param stateValue stateValue
+     */
+    State(String stateName, int stateValue){
+        this.stateName = stateName;
+        this.stateValue = stateValue;
+    }
+
+    /**
+     * This method changes stateValue.
+     * @param newValue stateValue
+     */
+    public void changeState(int newValue){
+        stateValue = newValue;
+    }
+
+    /**
+     * This method returns stateName;
+     * @return stateName
+     */
+    public String getStateName(){
+        return stateName;
+    }
+
+    /**
+     * This method returns stateValue.
+     * @return stateVaule
+     */
+    public int getStateValue(){
+        return stateValue;
+    }
+
+    /**
+     * This method returns string:"stateName -> stateValue"
+     * @return toString
+     */
+    public String toString(){
+        return stateName + "->" + stateValue;
+    }
+
+    public State clone(){
+        return new State(stateName, stateValue);
+    }
+
+    /**
+     * This method compares two states, returns true if they have same stateName and stateValue.
+     * @param aState another state
+     * @return is equal
+     */
+    public boolean equals(State aState){
+        if (this.stateName.equals(aState.getStateName()) && this.stateValue == aState.getStateValue()){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/src/host/Timer.java
@@ -0,0 +1,12 @@
+package host;
+
+/**
+ * Created by owen on 5/26/17.
+ */
+public class Timer {
+    long starts;
+
+    public void Start() {
+        starts = System.currentTimeMillis();
+    }
+}
