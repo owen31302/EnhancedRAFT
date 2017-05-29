1ab20b7e5a0c2472b8211840930cf6e08f0aa0bb
tyeh11 <tyeh75@gmail.com>
2017-05-29 09:41:04 -0700

    Add package

--- src/src/ClientInstructionCode.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by yujian on 5/29/17.
- */
-public class ClientInstructionCode {
-    static int AddHostAddresses = 0;
-    static int Ackowledgement = 1;
-    static int EnableByzantine = 2;
-    static int DisableByzantine = 3;
-}
